e99dd21800948ca63cf4f9d2ace6c1ad26f1c34f
tamires <tamireshc@gmail.com>
2022-10-27 18:09:27 -0300

    change fav and title

--- src/services/requests.js
+++ src/services/requests.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://tfc-tamires-bc.herokuapp.com',
+  baseURL: `http://tfc-tamires-bc.herokuapp.com`,
 });
 
 export const setToken = (token) => {
